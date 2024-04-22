6acdc49bbc9f886f06c774a58932bbc705c31468
Abhishek <abhisawant111111@gmail.com>
2024-04-22 10:49:42 +0530

    abc

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ hamburgers.forEach((hamburger) =>
   })
 );
 // === === === === Navbar logic === === === ===
+
+let points = document.querySelectorAll('.point');
+let details = document.querySelectorAll('.detail');
+
+points.forEach((point, index) => {
+  point.addEventListener('click', () => {
+    points.forEach((point) => point.classList.remove('point--active'));
+    point.classList.toggle('point--active');
+    details[index].classList.toString('detail-toggle');
+  });
+});
+
+let accordionItems = document.querySelectorAll('.accordion__item');
+let accordionHeaders = document.querySelectorAll('.accordion__header');
+
+accordionHeaders.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    accordionItems.forEach((item) =>
+      item.classList.remove('accordion__item--active')
+    );
+    accordionItems[index].classList.add('accordion__item--active');
+  });
+});
